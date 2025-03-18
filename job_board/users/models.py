1367262cfe6fffa7927830ab908b62d44343fb3e
Drew <ayobamioduola13@gmail.com>
2025-03-19 00:37:53 +0100

    feat(users): login with email

--- job_board/users/models.py
+++ job_board/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import CustomUserManager
+    
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name='email', unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_employer = models.BooleanField(default=False)
+    is_job_seeker = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return super().__str__()
+    
 
-# Create your models here.
+
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='employer_profile')
+    company_name = models.CharField(max_length=255)
+    company_description = models.TextField()
+
+class JobSeekerProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='job_seeker_profile')
+    bio = models.TextField()
+    skills = models.CharField(max_length=255)
